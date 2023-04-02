a48733d44f1ef8703c81f2b522ca2ad5a1424adc
MuneimAsif <muneimasif2004@gmail.com>
2023-04-02 02:36:15 -0700

    This is my first repo

---
+++ app.js
@@ -0,0 +1,86 @@
+window.alert("hello world");
+
+var a = "Ramadan Mubarak";
+var b = "Khair Mubarak";
+var c = a + b;
+window.alert(c);
+ 
+var a = "Eid Mubarak";
+var b = "Picnic pe jaaenge eid me";
+var c = a + b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = 10;
+var d = 5;
+var e = 12;
+var f = 11;
+var x = (a + e)*b - c / f + d / e
+console.log(x)
+
+var a = 10;
+var b = "20";
+var c = a / b;
+console.log(c)
+
+var a = 10;
+var b = "20";
+var c = a - b;
+console.log(c)
+
+var a = 10;
+var b = "20";
+var c = a * b;
+console.log(c)
+
+// =============This is not possible While adding
+
+var a = 10;
+var b = "20";
+var c = a + b;
+console.log(c)
+
+// =========== x ============ x =============
+
+
+var a = "MUNEIM ASIF ";
+var b = "PATEL";
+var c = a + b;
+console.log(c);
+
+var a = "abc";
+var b = 10;
+var c = a + b;
+console.log(c)
+
+
+
+
+var a = 10;
+a = 10 + 1;
+a++;
+++a
+a--
+--a
+var b = a++;
+var b = ++a;
+console.log(b)
+
+var a = 10;
+a++;
+b = a++;
+console.log(b);
+
+var b = 20;
+b--;
+c = b--
+console.log(c)
+
+
+var name = prompt("Enter name");
+var fathername = prompt("Enter Father' Name");
+var email = prompt("Enter Email");
+var city = prompt("Enter City");
+var fb = prompt("Enter Facebook id")
+console.log(name + " " + fathername + " " + email + " " + city + " " + fb)
